a126f023d917e1e303321709981707c6e96c31b6
francesca-golzio <francescagolzio@gmail.com>
2026-07-31 17:26:54 +0200

    fix(db): add columns to trains table
    - add missing departure_time column
    - add departure/arrival_date columns

---
+++ database/migrations/2026_07_31_152317_add_departure_day_and_arrival_day_to_trains_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->date('departure_day')->after('arrival_time');
+            $table->date('arrival_day')->after('departure_day');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->dropColumn('departure_day');
+            $table->dropColumn('arrival_day');
+        });
+    }
+};
