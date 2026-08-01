4388228147e97d2a8374136c0501e0b50ecf6b5a
francesca-golzio <francescagolzio@gmail.com>
2026-08-01 11:46:56 +0200

    fix: index route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\TrainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [TrainController::class, 'index'])->name('index');
