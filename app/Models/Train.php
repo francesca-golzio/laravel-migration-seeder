2f7dc194af7300336145f74c120d1ab2e2fba0de
francesca-golzio <francescagolzio@gmail.com>
2026-08-02 16:10:54 +0200

    update Train Model
    - add delay cast
    - remove year from departure_date
    - add functions:
     - getArrivalDay()
     - getArrivalTime()
     - getExpectedDepartureTime()
     - getExpectedArrivalTime()

--- app/Models/Train.php
+++ app/Models/Train.php
@@ -9,17 +9,66 @@ class Train extends Model
     protected $casts = [
         'departure_date' => 'datetime',
         'arrival_date' => 'datetime',
+        'delay' => 'datetime:H:i:s',
         'is_on_time' => 'boolean',
         'is_cancelled' => 'boolean',
     ];
 
     public function getDepartureDay() {
-        $departure_day = $this->departure_date->format('Y M d');
+        $departure_day = $this->departure_date->format('M d');
         return $departure_day;
     }
-
+    
     public function getDepartureTime() {
         $departure_time = $this->departure_date->format('h:i A');
         return $departure_time;
+    }
+    
+    public function getArrivalDay() {
+        $arrival_day = $this->arrival_date->format('M d');
+        return $arrival_day;
+    }
+
+    public function getArrivalTime() {
+        $arrival_time = $this->arrival_date->format('h:i A');
+        return $arrival_time;
+    }
+
+    public function getExpectedDepartureTime() {
+        if ($this->is_on_time == false && $this->is_cancelled == false) {
+
+            $expected_departure_time_if_delay = 
+                $this->departure_date
+                ->copy()
+                ->addHours($this->delay->hour)
+                ->addMinutes($this->delay->minute)
+                ->addSeconds($this->delay->second)
+                ->format('h:i A');
+
+            return $expected_departure_time_if_delay;
+
+        } else {
+            return "-";
+        }
+
+    }
+
+    public function getExpectedArrivalTime() {
+        if ($this->is_on_time == false && $this->is_cancelled == false) {
+
+            $expected_arrival_time_if_delay = 
+                $this->arrival_date
+                ->copy()
+                ->addHours($this->delay->hour)
+                ->addMinutes($this->delay->minute)
+                ->addSeconds($this->delay->second)
+                ->format('h:i A');
+
+            return $expected_arrival_time_if_delay;
+
+        } else {
+            return "-";
+        }
+
     }
 }
