0432467ef06980530559afa1e63c50e4071705f1
francesca-golzio <francescagolzio@gmail.com>
2026-08-03 09:42:22 +0200

    feat(ui): improve ui
    - add bg image
    - add header
    - change sample title into 'The Magical Journey'
    - fold content into <main> > container
    - add styling rules
    - add footer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,66 +4,88 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite (['resources/sass/app.scss', 'resources/js/app.js'])
-  <title>laravel-migration-seeder</title>
+  <title>The Magical Journey</title>
 </head>
 <body>
-  <h1>laravel-migration-seeder</h1>
 
-  <caption>
-    <div class="d-flex">
-      <h2>London King's Cross</h2>
-      <h3>departures</h3>
+  <header>
+    <div class="container p-2">
+      <h1>The Magical Journey</h1>
     </div>
-  </caption>
-  <div class="table-responsive">
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">date</th>
-          <th scope="col">company</th>
-          <th scope="col">destination</th>
-          <th scope="col">dep</th>
-          <th scope="col">exp</th>
-          <th scope="col">plat</th>
-          <th scope="col">status</th>
-          <th scope="col">arr</th>
-          <th scope="col">exp</th>
-          <th scope="col">coach</th>
-        </tr>
-      </thead>
-      <tbody>
-      @foreach ($trains as $train)
-        @if (!$train->departure_date->isPast())
-          <tr class="">
-            <td>{{ $train->getDepartureDay() }}</td>
-            <td scope="row">{{ $train->company }}</td>
-            <td>{{ $train->arrival_station }}</td>
-            <td>{{ $train->getDepartureTime() }}</td>
-            <td>{{ $train->getExpectedDepartureTime() }}</td>
-            <td>{{ $train->platform }}</td>
-            <td>
-              @if ($train->is_cancelled)
-              cancelled
-              @else
-                @if ($train->is_on_time)
-                on time
-                @else
-                delayed
-                @endif
-              @endif
-            </td>
-            <td>{{ $train->getArrivalTime() }}</td>
-            <td>{{ $train->getExpectedArrivalTime() }}</td>
-            <td>{{ $train->carriages }}</td>
-          </tr>
-        @endif
-      @endforeach  
-      </tbody>
-    </table>
-  </div>
-
-
+  </header>
   
+  <main>
+    <div class="container text-uppercase">
+      <div class="table_and_caption">
+        <caption>
+          <div class="caption_title d-flex justify-content-between text-bg-dark">
+            <h2>London King's Cross</h2>
+            <h3>departures</h3>
+          </div>
+        </caption>
+        <table class="table table-borderless table-dark table-striped">
+          <thead>
+            <tr>
+              <th scope="col"><div class="px-2">date</div></th>
+              <th scope="col"><div class="px-2">company</div></th>
+              <th scope="col"><div class="px-2">destination</div></th>
+              <th scope="col"><div class="px-2">dep</div></th>
+              <th scope="col" class="text-center"><div class="px-2">exp</div></th>
+              <th scope="col class="text-center""><div class="px-2">plat</div></th>
+              <th scope="col"><div class="px-2">status</div></th>
+              <th scope="col"><div class="px-2">arr</div></th>
+              <th scope="col" class="text-center"><div class="px-2">exp</div></th>
+              <th scope="col" class="text-center"><div class="px-2">coach</div></th>
+            </tr>
+          </thead>
+          <tbody>
+          @foreach ($trains as $train)
+            @if (!$train->departure_date->isPast())
+              <tr class="">
+                <td><div class="px-2">{{ $train->getDepartureDay() }}</div></td>
+                <td scope="row"><div class="px-2">{{ $train->company }}</div></td>
+                <td><div class="px-2">{{ $train->arrival_station }}</div></td>
+                <td><div class="px-2">{{ $train->getDepartureTime() }}</div></td>
+                <td class="text-center"><div class="px-2">{{ $train->getExpectedDepartureTime() }}</div></td>
+                <td class="text-center"><div class="px-2">{{ $train->platform }}</div></td>
+                <td>
+                  <div class="px-2">
+                  @if ($train->is_cancelled)
+                    <div><mark class="cancelled">cancelled</mark></div>
+                  @else
+                    @if ($train->is_on_time)
+                    <div><mark class="on_time">on time</mark></div>
+                    @else
+                    <div><mark class="delayed">delayed</mark></div>
+                    @endif
+                  @endif
+                  </div>
+                </td>
+                <td><div class="px-2">{{ $train->getArrivalTime() }}</div></td>
+                <td class="text-center"><div class="px-2">{{ $train->getExpectedArrivalTime() }}</div></td>
+                <td class="text-center"><div class="px-2">{{ $train->carriages }}</div></td>
+              </tr>
+            @endif
+          @endforeach  
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </main>
+
+  <footer>
+    <div class="container p-2">
+      <div class="text-center">2026 &copy; The Magical Journey<br>~ Francesca Golzio ~</div>
+      <div class=" img_credits text-end">
+        Credits: <a href="https://commons.wikimedia.org/w/index.php?curid=19363732">background image</a> by &copy;
+        <a href="//commons.wikimedia.org/wiki/User:Colin" title="User:Colin">Colin</a>
+        &nbsp;/&nbsp;
+        <a href="//commons.wikimedia.org/wiki/Main_Page" title="Main Page">Wikimedia Commons</a>
+        &nbsp;/&nbsp; 
+        <a href="https://creativecommons.org/licenses/by-sa/3.0" title="Creative Commons Attribution-Share Alike 3.0">CC BY-SA 3.0</a>
+      </div>
+    </div>
+  </footer> 
 
 </body>
 </html>
