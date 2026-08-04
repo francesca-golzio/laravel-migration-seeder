1505ff9c13a7c57a84e3149feab7057600fe2fd2
francesca-golzio <francescagolzio@gmail.com>
2026-08-04 12:24:08 +0200

    fix(ui): fix responsiveness

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,59 +16,61 @@
   
   <main>
     <div class="container text-uppercase">
-      <div class="table_and_caption">
+      <div class="table_and_caption d-flex flex-column">
         <caption>
           <div class="caption_title d-flex justify-content-between text-bg-dark">
             <h2>London King's Cross</h2>
             <h3>departures</h3>
           </div>
         </caption>
-        <table class="table table-borderless table-dark table-striped">
-          <thead>
-            <tr>
-              <th scope="col"><div class="px-2">date</div></th>
-              <th scope="col"><div class="px-2">train &middot; company</div></th>
-              <th scope="col"><div class="px-2">from</div></th>
-              <th scope="col"><div class="px-2">destination</div></th>
-              <th scope="col"><div class="px-2">dep</div></th>
-              <th scope="col"><div class="px-2">exp</div></th>
-              <th scope="col"><div class="px-2">plat</div></th>
-              <th scope="col"><div class="px-2">status</div></th>
-              <th scope="col"><div class="px-2">arr</div></th>
-              <th scope="col" class="text-center"><div class="px-2">exp</div></th>
-              <th scope="col" class="text-center"><div class="px-2">coach</div></th>
-            </tr>
-          </thead>
-          <tbody>
-          @foreach ($trains as $train)
-            <tr class="">
-              <td><div class="px-2">{{ $train->getDepartureDay() }}</div></td>
-              <td scope="row"><div class="px-2">{{ $train->train_code }} &middot; {{ $train->company }}</div></td>
-              <td><div class="px-2">{{ $train->departure_station }}</div></td>
-              <td><div class="px-2">{{ $train->arrival_station }}</div></td>
-              <td><div class="px-2">{{ $train->getDepartureTime() }}</div></td>
-              <td class="text-center"><div class="px-2">{{ $train->getExpectedDepartureTime() }}</div></td>
-              <td class="text-center"><div class="px-2">{{ $train->platform }}</div></td>
-              <td>
-                <div class="px-2">
-                @if ($train->is_cancelled)
-                  <div><mark class="cancelled">cancelled</mark></div>
-                @else
-                  @if ($train->is_on_time)
-                  <div><mark class="on_time">on time</mark></div>
+        <div class="table-responsive">
+          <table class="table table-borderless table-dark table-striped">
+            <thead>
+              <tr>
+                <th scope="col"><div class="px-2">date</div></th>
+                <th scope="col"><div class="px-2">train &middot; company</div></th>
+                <th scope="col"><div class="px-2">from</div></th>
+                <th scope="col"><div class="px-2">destination</div></th>
+                <th scope="col"><div class="px-2">dep</div></th>
+                <th scope="col"><div class="px-2">exp</div></th>
+                <th scope="col"><div class="px-2">plat</div></th>
+                <th scope="col"><div class="px-2">status</div></th>
+                <th scope="col"><div class="px-2">arr</div></th>
+                <th scope="col" class="text-center"><div class="px-2">exp</div></th>
+                <th scope="col" class="text-center"><div class="px-2">coach</div></th>
+              </tr>
+            </thead>
+            <tbody>
+            @foreach ($trains as $train)
+              <tr class="">
+                <td><div class="px-2">{{ $train->getDepartureDay() }}</div></td>
+                <td scope="row"><div class="px-2">{{ $train->train_code }} &middot; {{ $train->company }}</div></td>
+                <td><div class="px-2">{{ $train->departure_station }}</div></td>
+                <td><div class="px-2">{{ $train->arrival_station }}</div></td>
+                <td><div class="px-2">{{ $train->getDepartureTime() }}</div></td>
+                <td class="text-center"><div class="px-2">{{ $train->getExpectedDepartureTime() }}</div></td>
+                <td class="text-center"><div class="px-2">{{ $train->platform }}</div></td>
+                <td>
+                  <div class="px-2">
+                  @if ($train->is_cancelled)
+                    <div><mark class="cancelled">cancelled</mark></div>
                   @else
-                  <div><mark class="delayed">delayed</mark></div>
+                    @if ($train->is_on_time)
+                    <div><mark class="on_time">on time</mark></div>
+                    @else
+                    <div><mark class="delayed">delayed</mark></div>
+                    @endif
                   @endif
-                @endif
-                </div>
-              </td>
-              <td><div class="px-2">{{ $train->getArrivalTime() }}</div></td>
-              <td class="text-center"><div class="px-2">{{ $train->getExpectedArrivalTime() }}</div></td>
-              <td class="text-center"><div class="px-2">{{ $train->carriages }}</div></td>
-            </tr>
-          @endforeach  
-          </tbody>
-        </table>
+                  </div>
+                </td>
+                <td><div class="px-2">{{ $train->getArrivalTime() }}</div></td>
+                <td class="text-center"><div class="px-2">{{ $train->getExpectedArrivalTime() }}</div></td>
+                <td class="text-center"><div class="px-2">{{ $train->carriages }}</div></td>
+              </tr>
+            @endforeach  
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   </main>
